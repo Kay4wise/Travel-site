8032cff8fd4bae8c6c0c280a750cc1bc2370f4a3
Kay4wise <159776838+Kay4wise@users.noreply.github.com>
2024-06-27 09:19:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+let searchBtn = document.getElementById('#search-btn');
+let searchBar = document.getElementById('.search-bar-container');
+let formBtn = document.getElementsById('#login-btn');
+let loginForm = document.getElementsByClassName('.login-form-container');
+let formClose = document.getElementsByClassName('form-close');
+let menu =document.getElementById('#menu-bar')
+let navbar = document.getElementsByClassName('.navbar');
+let videoBtn = document.getElementsByClassName('.vid-btn');
+
+window.onscroll = () =>{
+    searchButton.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    
+});
+
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa times');
+    searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5
+        },
+    },
+});
+
